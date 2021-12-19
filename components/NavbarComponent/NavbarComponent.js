ea1045dd43690842df148daa3b394d7f9178c00c
Jareem Hoff <jareemhoff@Jareems-MBP.lan>
2021-12-19 16:04:21 -0500

    started building out the navbarcomponent

---
+++ components/NavbarComponent/NavbarComponent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function NavbarComponent() {
+    return (
+        <div className="navbar-container">
+            
+
+
+            <style jsx>{`
+                .navbar-container {
+                    width: 100%;
+                    height: 45px;
+                    background
+                }
+            `}</style>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import NavbarComponent from '../components/NavbarComponent/NavbarComponent'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <NavbarComponent />
     </div>
   )
 }
